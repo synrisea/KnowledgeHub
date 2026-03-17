42bb717a10cfa825d3ecc0cff47d59f751639d3d
zamanof <zamanov@itstep.edu.az>
2026-03-17 21:53:52 +0400

    Lesson 4

--- Django 03/Django 03. Closure.py
+++ Django 03/Django 03. Closure.py
@@ -6,3 +6,20 @@ def outer(...):
     return inner
 """
 
+def make_counter():
+    count = 0
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+clicks= make_counter()
+print(clicks())
+print(clicks())
+print(clicks())
+print(clicks())
+
+
+
+
